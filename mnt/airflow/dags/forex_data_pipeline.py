cbb67d48a5761a1ed980e51195e1a6e6489676bb
seilylook <seilylook@naver.com>
2024-06-04 18:23:28 +0900

    feat: file upload task

--- mnt/airflow/dags/forex_data_pipeline.py
+++ mnt/airflow/dags/forex_data_pipeline.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.sensors.filesystem import FileSensor
 from datetime import datetime, timedelta
 
 default_args = {
@@ -19,11 +20,21 @@ with DAG(
     catchup=False,
 ) as dag:
     
+    # Http connection Task
     is_forex_rates_available = HttpSensor(
         task_id="is_forex_rates_available",
         http_conn_id="forex_api",
         endpoint="marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b",
         response_check=lambda response: "rates" in response.text, 
+        poke_interval=5,
+        timeout=20,
+    )
+    
+    # File upload Task
+    is_forex_currencies_file_available = FileSensor(
+        task_id="is_forex_currencies_file_available",
+        fs_conn_id="forex_path",
+        filepath="forex_currencies.csv",
         poke_interval=5,
         timeout=20,
     )
