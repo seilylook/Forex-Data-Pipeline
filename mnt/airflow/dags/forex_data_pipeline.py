c1e062b5e75a45799a9e02665a64571cc4be4edc
seilylook <seilylook@naver.com>
2024-06-05 14:21:59 +0900

    feat: create hive talbe forex rates

--- mnt/airflow/dags/forex_data_pipeline.py
+++ mnt/airflow/dags/forex_data_pipeline.py
@@ -3,6 +3,7 @@ from airflow.providers.http.sensors.http import HttpSensor
 from airflow.sensors.filesystem import FileSensor
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
+from airflow.providers.apache.hive.operators.hive import HiveOperator
 
 from datetime import datetime, timedelta
 import csv
@@ -81,5 +82,26 @@ with DAG(
         bash_command="""
             hdfs dfs -mkdir -p /forex && \
             hdfs dfs -put -f $AIRFLOW_HOME/dags/files/forex_rates.json /forex
+        """,
+    )
+
+    # Create the Hive table forex rates - HiveOperator
+    creating_forex_rates_table = HiveOperator(
+        task_id="creating_forex_rates_table",
+        hive_cli_conn_id="hive_conn",
+        hql="""
+            CREATE EXTERNAL TABLE IF NOT EXISTS forex_rates(
+                base STRING,
+                last_update DATE,
+                eur DOUBLE,
+                usd DOUBLE,
+                nzd DOUBLE,
+                gbp DOUBLE,
+                jpy DOUBLE,
+                cad DOUBLE
+            )
+            ROW FORMAT DELIMITED
+            FIELDS TERMINATED BY ','
+            STORED AS TEXTFILE
         """,
     )
